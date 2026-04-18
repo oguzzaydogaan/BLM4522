4e1394e80446c97ebaa239fa2ddf58185e0af2b6
Oğuzhan Aydoğan <negatif25@gmail.com>
2026-04-18 20:33:54 +0300

    veri eklendi ve fark yedeklemesi alındı.

---
+++ 05_Import_Datas_For_Diff.sql
@@ -0,0 +1,7 @@
+USE BLM4522;
+GO
+
+INSERT INTO Musteriler (Ad, Soyad, Email) VALUES
+('DiffMusteri1', 'DiffSoyad1', 'diffuser1@mail.com'),
+('DiffMusteri2', 'DiffSoyad2', 'diffuser2@mail.com'),
+('DiffMusteri3', 'DiffSoyad3', 'diffuser3@mail.com');
