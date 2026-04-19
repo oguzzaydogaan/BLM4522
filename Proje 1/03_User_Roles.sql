a3e042714b88f93d3a0c86a5efe57e3cb77002a6
Oğuzhan Aydoğan <negatif25@gmail.com>
2026-04-19 16:05:55 +0300

    örnek senaryo eklendi ve proje raporu tamamlandı.

--- Proje 1/03_User_Roles.sql
+++ Proje 1/03_User_Roles.sql
@@ -1,8 +1,8 @@
-CREATE LOGIN StajyerAnalizci WITH PASSWORD = 'AnalizSifre123!';
+CREATE LOGIN StajyerLogin WITH PASSWORD = '1234';
 
 USE BLM4522;
-CREATE USER StajyerUser FOR LOGIN StajyerAnalizci;
+CREATE USER Stajyer FOR LOGIN StajyerLogin;
 
-ALTER ROLE db_datareader ADD MEMBER StajyerUser;
+ALTER ROLE db_datareader ADD MEMBER Stajyer;
 
-ALTER ROLE db_datawriter ADD MEMBER StajyerUser;
+ALTER ROLE db_datawriter ADD MEMBER Stajyer;
---
+++ Proje 1/04_User_Role_Examples.sql
@@ -0,0 +1,5 @@
+SELECT TOP 10 * FROM Musteriler;
+
+DELETE FROM Musteriler WHERE MusteriID = 1; 
+
+DROP TABLE Musteriler;
